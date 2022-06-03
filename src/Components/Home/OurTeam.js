b56f1cbc51cd79ac3747eebbb94d0699ca6df2e1
SabbirSami <smd71430@gmail.com>
2022-06-03 12:40:45 +0600

    make join our team responsive

--- src/Components/Home/OurTeam.js
+++ src/Components/Home/OurTeam.js
@@ -4,29 +4,29 @@ import ourTeamBottom from "../../Images/ourTeamBottom.webp";
 const OurTeam = () => {
     return (
         <section className="our-team-section mt-5">
-            <div className="container position-relative ">
-                <div className="py-5">
-                    <div className="py-lg-5">
-                        <div className="row">
-                            <div className="col-lg-6">
-                                <div className="text-light py-5">
-                                    <div className="py-5">
-                                        <h5>Join Our Team</h5>
-                                        <div className="col-lg-12">
-                                            <h1 className="fontStyle pb-4">
-                                                It takes the world's best talent
-                                                to change the game.
-                                            </h1>
-                                        </div>
-                                        <button className="btn btn-warning rounded-4 py-3 px-4">
-                                            Explore Careers
-                                        </button>
-                                    </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <div className="text-light py-5">
+                            <div className="py-5">
+                                <h5>Join Our Team</h5>
+                                <div className="col-lg-12">
+                                    <h1 className="fontStyle pb-4">
+                                        It takes the world's best talent to
+                                        change the game.
+                                    </h1>
                                 </div>
+                                <button className="btn btn-warning rounded-4 py-3 px-4">
+                                    Explore Careers
+                                </button>
                             </div>
-                            <div className="col-lg-6 ">
-                                <div className="py-lg-5">
-                                    <div className="position-absolute bottom-0 end-0">
+                        </div>
+                    </div>
+                    <div className="col-lg-6 ">
+                        <div className="align-self-end">
+                            <div className="pt-lg-5">
+                                <div className="pt-lg-5">
+                                    <div className="pt-lg-5">
                                         <img
                                             className=""
                                             src={ourTeamBottom}
