ead2892cbe94d33951e447e843f9ac2b346a6f56
SabbirSami <smd71430@gmail.com>
2022-06-04 16:26:16 +0600

    what-we-are-section done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,10 +1,12 @@
 import React from "react";
 import WeAreTripo from "./WeAreTripo";
+import WhatWeAreAbout from "./WhatWeAreAbout";
 
 const About = () => {
     return (
         <div>
             <WeAreTripo></WeAreTripo>
+            <WhatWeAreAbout></WhatWeAreAbout>
         </div>
     );
 };
---
+++ src/Components/About/WhatWeAreAbout.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const WhatWeAreAbout = () => {
+    return (
+        <div className="what-we-are-section pb-5">
+            <div className="container py-5">
+                <div className="col-lg-9 mx-auto">
+                    <div className="row">
+                        <h1 className="fontStyle py-5 text-center">
+                            What We Are About
+                        </h1>
+                        <div className=" py-3 col-lg-6">
+                            <div className="p-5 shadow rounded-4">
+                                <h2 className="text-danger">Accomplishment</h2>
+                                <p>
+                                    I'm a paragraph. Click here to add your own
+                                    text and edit me. I’m a great place for you
+                                    to tell a story and let your users know a
+                                    little more about you.
+                                </p>
+                            </div>
+                        </div>
+                        <div className=" py-3 col-lg-6">
+                            <div className="p-5 shadow rounded-4">
+                                <h2 className="text-danger">Creativity</h2>
+                                <p>
+                                    I'm a paragraph. Click here to add your own
+                                    text and edit me. I’m a great place for you
+                                    to tell a story and let your users know a
+                                    little more about you.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-lg-12 py-3">
+                            <div className="p-5 rounded-4 shadow">
+                                <h2 className="text-danger">Community</h2>
+                                <p>
+                                    I'm a paragraph. Click here to add your own
+                                    text and edit me. I’m a great place for you
+                                    to tell a story and let your users know a
+                                    little more about you.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WhatWeAreAbout;
